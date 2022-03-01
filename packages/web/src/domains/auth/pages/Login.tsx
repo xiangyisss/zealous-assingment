401e9d33575f96c3da7198160347d21e99a7c9de
Michiel <michiel_87@hotmail.com>
2022-03-01 12:23:16 +0100

    chore: use placeholder

--- packages/web/src/domains/auth/pages/Login.tsx
+++ packages/web/src/domains/auth/pages/Login.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import { Panel, Input } from '@zealous/ui'
-import { useState } from 'react'
 
 const Layout = styled.div`
   display: flex;
@@ -12,13 +11,10 @@ const Layout = styled.div`
 `
 
 export function LoginPage() {
-  const [username, setUsername] = useState('username')
   return (
     <Layout>
       <Panel>
-        <Input
-          value={username}
-          onChange={setUsername}/>
+        <Input placeholder='email' />
       </Panel>
     </Layout>
   )
